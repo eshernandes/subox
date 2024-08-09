5c33afa137af2c3490fecf435a402cc98bceb418
Estevan Hernandes Silva de Oliveira <estevan.hernandes@aluno.ifsp.edu.br>
2024-08-09 20:06:52 -0300

    criação inicial do projeto e .gitignore

---
+++ Meta 2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf(" ___       ___          \n");
+    printf("/ __> _ _ | . > ___ __  \n");
+    printf("\\__ \\| | || . \\/ . \\\\/\\/\n");
+    printf("<___/`___||___/\\___//\\_\\\n");
+    printf("                        \n");
+
+    return 0;
+}
